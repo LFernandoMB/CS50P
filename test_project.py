e632f20567421503ce3cd3fea9a41017b0e6aa34
Luis Fernando M Bezerra <91624923+LFernandoMB@users.noreply.github.com>
2023-10-09 23:59:54 +0100

    feat: Finish

--- test_project.py
+++ test_project.py
@@ -1,19 +1,30 @@
+from project import get_data_segmentos, get_data_online, save_data
+
 def main():
-    test_function1()
-    test_function2()
-    test_function3()
+    test_get_data_segmentos()
+    test_get_data_online()
+    test_save_data()
 
 
-def test_function1():
-    pass
+def test_get_data_segmentos():
+    data = get_data_segmentos()
+    assert len(data) > 0
 
 
-def test_function2():
-    pass
+def test_get_data_online():
+    data = get_data_online("acoes", "PETR4", "OIL AND GAS")
+    assert len(data) == 9
 
 
-def test_function3():
-    pass
+def test_save_data():
+    database = [['PETR4', 'OIL AND GAS', 'Petrobras', '28,49', '11,64%', '24/08/21', '16/09/21', '0,23', '2023-02-20 17:12:13']]
+    save_data(database)
+    try:
+        with open('online_action.csv', 'r', encoding='utf8') as f:
+            content = f.read()
+            assert len(content) > 0
+    except FileNotFoundError:
+        assert False
 
 
 if __name__ == "__main__":
